2aa9e12b432847aea2dc6deb6b3382a99e55192d
safrizalrahman46 <safrizalrahman46@gmail.com>
2025-04-25 12:57:53 +0700

    push

---
+++ UPA/resources/js/Pages/Home/Index.vue
@@ -0,0 +1,27 @@
+<script setup>
+// import Layout from './Layout'
+import { Head } from '@inertiajs/vue3'
+import { Button } from '@/components/ui/button'
+
+
+defineProps({
+  user: Object,
+//   Button: defineComponent
+
+})
+// defineExpose({
+//   Button: defineComponent({})
+// })
+</script>
+
+<template>
+  <!-- <Layout> -->
+  <Head title="Welcome" />
+  <h1 class="text-blue-500 text-center p-5">Welcome</h1>
+  <Button>Click Me</Button>
+  <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+    <slot />
+  </button>
+  <!-- <p>Hello {{ user.name }}, welcome to your first Inertia app!</p> -->
+  <!-- </Layout> -->
+</template>
--- UPA/vite.config.js
+++ UPA/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
     plugins: [
+        vue(),
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
